4da50782c9a4b5b491cc93b56cf8e8ae91bea61a
Kasper Siig <kasp9250@easv365.dk>
2019-03-22 16:51:32 +0100

    meh

--- install.py
+++ install.py
@@ -2,4 +2,6 @@ import os
 
 dir = os.popen('pwd').read()
 
+os.system('sudo apt update; sudo apt install zsh -y')
+os.system('chsh -s $(which zsh)')
 os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"')
