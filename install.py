ebf8652063bae151fe6e212e1ac53607c86ac41b
Kasper Siig <kasp9250@easv365.dk>
2019-03-22 17:19:21 +0100

    meh

--- install.py
+++ install.py
@@ -4,6 +4,6 @@ dir = os.popen('pwd').read()
 
 os.system('sudo apt update; sudo apt install zsh -y')
 os.system('chsh -s $(which zsh)')
-os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"')
+os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";exit')
 os.system('git clone https://github.com/denysdovhan/spaceship-prompt.git "~/.oh-my-zsh/themes/spaceship-prompt"')
 os.system('ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "~/.oh-my-zsh/themes/spaceship.zsh-theme"')
