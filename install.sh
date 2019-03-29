d518ee9d27f47c4ae544136c048b68f077158865
Kasper Siig <kasp9250@easv365.dk>
2019-03-29 07:29:19 +0100

    meh

--- install.sh
+++ install.sh
@@ -20,4 +20,4 @@ fi
 
 chsh -s $(which zsh)
 
-sh -c "$(https://raw.githubusercontent.com/KasperSiig/oh-my-zsh/master/tools/install.sh)"'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/KasperSiig/oh-my-zsh/master/tools/install.sh)"'
